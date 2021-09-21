814d1ac8a42b2e7692a9106b3d787b7393b10abc
terizah <88724711+terizah@users.noreply.github.com>
2021-09-21 20:55:07 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php
 
+$title = "Courses";
+include('includes/header.inc');
+?>
+<h1>About our College</h1>
+<?php
+include('includes/nav.inc');
+?>
+<table>
+  <tr>
+  <th>Home</th>
+  <th>Courses</th>
+  <th>Subjects</th>
+  <th>Instuctors</th>
+  <th>Add</th>
+  </tr>
+  
+   
